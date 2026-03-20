d5880f044ee1a6cc57b45c5e6422bda83dec53bb
Akash Bhise <97794144+akashbhise67@users.noreply.github.com>
2026-03-20 09:11:00 +0530

    two folders

---
+++ java_practice/Geeks.java
@@ -0,0 +1,12 @@
+public class Geeks{
+    public void printMessage(){
+        System.out.println("Hello, Geeks!");
+    }
+    public static void main(String[] args){
+        Geeks obj = new Geeks();
+
+        obj.printMessage();
+        obj.printMessage();
+
+    }
+}
